0c1be8e6f22a7769e2ea1b75ae69e54b773e5b85
Sara Beth Ribot <sbribot@gmail.com>
2018-06-06 00:53:27 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,24 +1,39 @@
-#write out your code here
+# #write out your code here
+
+# def least_coins(cents)
+# coins = {:quarters => 0, :dimes => 0, :nickels => 0, :pennies => 0}
+# while cents >= 25
+# coins[:quarters] += 1 
+# cents = cents - 25
+# end
+# while cents >= 10 
+# coins[:dimes] += 1 
+# cents = cents - 10
+# end
+# while cents >= 5 
+# coins[:nickels] += 1 
+# cents = cents - 5
+# end
+# while cents >= 1 
+# coins[:pennies] += 1
+# cents = cents - 1
+# end
+# return coins
+# end
+
+# least_coins(29)
 
 def least_coins(cents)
-coins = {:quarters => 0, :dimes => 0, :nickels => 0, :pennies => 0}
-while cents >= 25
-coins[:quarters] += 1 
-cents = cents - 25
-end
-while cents >= 10 
-coins[:dimes] += 1 
-cents = cents - 10
-end
-while cents >= 5 
-coins[:nickels] += 1 
-cents = cents - 5
-end
-while cents >= 1 
-coins[:pennies] += 1
-cents = cents - 1
-end
-return coins
-end
+  coins = {:quarters => 0.0, :dimes => 0.0, :nickels => 0.0, :pennies => 0.0}
+  coins[:quarters] = cents/25 
+  cents = cents/25
+  coins[:dimes] = cents/10
+  cents = cents/10
+  coins[:nickels] = cents/5
+  cents = cents/5
+  coins[:pennies] = cents/1
+  cents = cents/1
+  puts coins 
+end 
 
-least_coins(29)
+least_coins(5)
